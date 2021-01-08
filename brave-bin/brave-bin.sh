13f5757706a19ed626f3090a4fcdbc4fdff0feb1
Lucas S. Beiler <lucasbeiler@protonmail.com>
2021-01-07 23:06:19 -0300

    brave-bin: Fix Brave's sandboxing mechanism for systems without unprivileged User Namespaces

--- brave-bin/brave-bin.sh
+++ brave-bin/brave-bin.sh
@@ -7,9 +7,10 @@ if [[ -f $USER_FLAGS_FILE ]]; then
    USER_FLAGS="$(cat $USER_FLAGS_FILE | sed 's/#.*//')"
 fi
 
-if [[ -f /proc/sys/kernel/unprivileged_userns_clone && $(< /proc/sys/kernel/unprivileged_userns_clone) == 0 ]]; then
-    >&2 echo "User namespaces are detected as disabled on your system, Brave will run with the sandbox disabled"
-    SANDBOX_FLAG="--no-sandbox"
-fi
+## Now Brave supports Chromium's SUID sandbox, so we don't depend on User Namespaces for sandboxing.
+# if [[ -f /proc/sys/kernel/unprivileged_userns_clone && $(< /proc/sys/kernel/unprivileged_userns_clone) == 0 ]]; then
+#     >&2 echo "User namespaces are detected as disabled on your system, Brave will run with the sandbox disabled"
+#     SANDBOX_FLAG="--no-sandbox"
+# fi
 
 exec /usr/lib/brave-bin/brave "$@" $SANDBOX_FLAG $PEPPER_FLASH_FLAG $USER_FLAGS
